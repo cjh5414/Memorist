024b7d269eea342f144e96ae7adc48143c85ef00
cjh5414 <cjh5414@gmail.com>
2017-11-06 12:00:15 +0900

    Set up server

--- memorist/memorist/settings.py
+++ memorist/memorist/settings.py
@@ -23,9 +23,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ["MEMORIST_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
+DEBUG = True
 ALLOWED_HOSTS = []
+
+if os.environ['MEMORIST_ENVIRONMENT'] == "production":
+    DEBUG = False
+    ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -140,6 +144,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
