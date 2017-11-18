3f5feb96913e36e7b4316be2297b09da4788c07b
cjh5414 <cjh5414@gmail.com>
2017-11-18 14:30:04 +0900

    Refactor migration

--- memorist/wordlist/migrations/0001_initial.py
+++ memorist/wordlist/migrations/0001_initial.py
@@ -17,11 +17,8 @@ class Migration(migrations.Migration):
             name='Word',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('created_time', models.DateTimeField(auto_now_add=True)),
-                ('modified_time', models.DateTimeField(auto_now=True)),
                 ('question', models.CharField(max_length=255)),
                 ('answer', models.CharField(max_length=255)),
-                ('is_delete', models.BooleanField(default=False)),
             ],
             options={
                 'abstract': False,
