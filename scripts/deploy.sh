8e91a292d8945b43efc82bd005708499334ecfe5
cjh5414 <cjh5414@gmail.com>
2018-02-03 11:37:53 -0800

    permission deploy sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,5 @@
-sh -p 23579 jihun@memorist.xyz -i ~/.ssh/id_memorist -o StrictHostKeyChecking=no <<'ENDSSH'
-./run_server.sh
+#!/bin/bash
+ssh -p 23579 jihun@memorist.xyz -i ~/.ssh/id_memorist -o StrictHostKeyChecking=no <<'ENDSSH'
+./Memorist/scripts/run_server.sh
 exit
 ENDSSH
