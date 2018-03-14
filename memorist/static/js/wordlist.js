48f2eb846bbf74f04e872b6248ee4db508546488
cjh5414 <cjh5414@gmail.com>
2018-03-14 01:04:52 -0700

    Put placeholder in wordlist edition input box

--- memorist/static/js/wordlist.js
+++ memorist/static/js/wordlist.js
@@ -8,8 +8,16 @@ $(".word_list_question, .word_list_answer").click(function() {
     tr = $(this).parent();
     tbody = tr.parent();
 
+    question = tr.find(".word_list_question").text();
+    answer = tr.find(".word_list_answer").text();
+
     tr.hide();
-    tbody.find(".word_list_input_row").show();
+
+    input_tr = tbody.find(".word_list_input_row");
+
+    input_tr.show();
+    input_tr.find(".word_list_input_question").val(question);
+    input_tr.find(".word_list_input_answer").val(answer);
 
     lastClickedWordTbody = tbody;
 });
