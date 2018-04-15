6969c076fe3f77ac22a22a03eb5e231a2cbe3ba2
cjh5414 <cjh5414@gmail.com>
2018-04-14 20:40:53 -0700

    Check password twice

--- memorist/account/forms.py
+++ memorist/account/forms.py
@@ -1,8 +1,21 @@
+from django import forms
 from django.forms import ModelForm
 from account.models import User
 
 
 class SignUpForm(ModelForm):
+    password1 = forms.CharField()
+    password2 = forms.CharField()
+
+    def clean_password2(self):
+        # Passwords must match
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError('The two password fields didn\'t match.')
+        return password2
+
     class Meta:
         model = User
-        fields = ['username', 'email', 'password']
+        fields = ['username', 'email', 'password1', 'password2']
+
