e265bdf16bafa9669a652c1d3a6e39ca51d4b93e
cjh5414 <cjh5414@gmail.com>
2018-05-01 02:26:56 -0700

    Fix not saveing password problem when signing up

--- memorist/account/forms.py
+++ memorist/account/forms.py
@@ -15,6 +15,13 @@ class SignUpForm(ModelForm):
             raise forms.ValidationError('The two password fields didn\'t match.')
         return password2
 
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
     class Meta:
         model = User
         fields = ['username', 'name', 'email', 'password1', 'password2']
