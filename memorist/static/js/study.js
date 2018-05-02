509e53bdbfb99e4f232b6a6023b798cbc85c9f7c
cjh5414 <cjh5414@gmail.com>
2018-05-02 01:36:17 -0700

    Refator study javascript logic

--- memorist/static/js/study.js
+++ memorist/static/js/study.js
@@ -90,27 +90,6 @@ $("#id_make_test_btn").click(function () {
     });
 });
 
-function onClickPronounce(event, source) {
-    event.stopPropagation();
-
-    if (source !== '' && source !== undefined) {
-        $.ajax({
-            type: "POST",
-            url: "/pronounce/",
-            data: {'question': source},
-            success: function (response) {
-                var audio = new Audio(MEDIA_URL + response.file_name);
-                audio.load();
-                audio.play();
-            },
-            error: function (request, status, error) {
-                console.log("code:" + request.status + "\n" + "message:" + request.responseText + "\n" + "error:" + error);
-                alert("API 요청 실패");
-            }
-        });
-    }
-}
-
 function onClickTestCol(index) {
     trs = $("#id_test_table, tr");
     test_answer = trs.eq(parseInt(index) + 2).find(".test_answer");
